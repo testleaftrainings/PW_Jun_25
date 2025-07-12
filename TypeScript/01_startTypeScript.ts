e17521ae9c90ff7d0cbcc10c256ea847cb140522
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-07-12 13:21:04 +0530

    Delete TypeScript directory

--- TypeScript/01_startTypeScript.ts
+++
@@ -1,4 +0,0 @@
-console.log("Learn TypeScript")
-//tsc 01_startTypeScript.ts
-//convert into js
-//node 01_startTypeScript.js
--- TypeScript/TSAssertion.js
+++
@@ -1,9 +0,0 @@
-//TS Assertion
-var price;
-price = 50;
-var empN = "Dilip";
-//type1 (price as number)
-//type2(<number>price)
-var assert = empN;
-console.log(empN);
-var assert1 = empN;
